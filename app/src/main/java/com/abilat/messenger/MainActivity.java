ea44e57a872336ac1ba488380c9dc1de6c5b73f2
Danya <Abilat01@yandex.ru>
2020-07-03 16:03:34 +0300

    Rest

---
+++ app/src/main/java/com/abilat/messenger/MainActivity.java
@@ -0,0 +1,32 @@
+package com.abilat.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSendMessage(View view) {
+
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("Text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chosenTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chosenTitle);
+        startActivity(chosenIntent);
+
+    }
+}
